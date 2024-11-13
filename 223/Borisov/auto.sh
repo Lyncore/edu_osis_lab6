36830148086bc26ada57c00599dce33cf29237e1
rdydryliu <borisov.evan@gmail.com>
2024-11-13 19:25:28 +0900

    Изменения, которые будут включены в коммит: При не указанной директории проект создаётся в данной

--- 223/Borisov/auto.sh
+++ 223/Borisov/auto.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-if ! [ -d $1 ]; then
-echo 'No directory'
-exit 0
+if [ $# -eq 1 ]; then
+mkdir $PWD/$1
+mkdir $PWD/$1/src
+mkdir  $PWD/$1/src/scripts
+mkdir $PWD/$1/src/styles
+mkdir $PWD/$1/src/images
+touch $PWD/$1/README.md
+touch $PWD/$1/gitignore
+echo "Используется для вывода текста и изображений" >> $PWD/$1/README.md
 fi
-mkdir $1/$2
-mkdir $1/$2/src
-mkdir  $1/$2/src/scripts
-mkdir $1/$2/src/styles
-mkdir $1/$2/src/images
-touch $1/$2/README.md
-touch $1/$2/gitignore
-echo "Используется для вывода текста и изображений" >> $1/$2/README.md
-echo "Проект $2 создался в дирректории $1"
+if [ $# -eq 2 ]; then
+mkdir $2/$1
+mkdir $2/$1/src
+mkdir  $2/$1/src/scripts
+mkdir $2/$1/src/styles
+mkdir $2/$1/src/images
+touch $2/$1/README.md
+touch $2/$1/.gitignore
+echo "Используется для вывода текста и изображений" >> $2/$1/README.md
+fi
+echo "Проект создан"
 exit 0
