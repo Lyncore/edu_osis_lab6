0800329e18e6b9602020e9bbaacc3318f40e4adc
rdydryliu <borisov.evan@gmail.com>
2024-11-15 20:26:25 +0900

    изменено:      auto.sh Оптимизировано создание дирректорий

--- 223/Borisov/auto.sh
+++ 223/Borisov/auto.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 if [ $# -eq 1 ]; then
-mkdir $PWD/$1
-mkdir $PWD/$1/src
-mkdir  $PWD/$1/src/scripts
-mkdir $PWD/$1/src/styles
-mkdir $PWD/$1/src/images
-touch $PWD/$1/README.md
-touch $PWD/$1/gitignore
-echo "Используется для вывода текста и изображений" >> $PWD/$1/README.md
+dir="$PWD/$1"
+mkdir -p $dir/src $dir/src/styles $dir/src/images
+echo "Используется для вывода текста и изображений" > $dir/README.md
+touch $dir/gitignore
 fi
 if [ $# -eq 2 ]; then
-mkdir $2/$1
-mkdir $2/$1/src
-mkdir  $2/$1/src/scripts
-mkdir $2/$1/src/styles
-mkdir $2/$1/src/images
-touch $2/$1/README.md
-touch $2/$1/.gitignore
-echo "Используется для вывода текста и изображений" >> $2/$1/README.md
+dir="$2/$1"
+mkdir -p $dir/src $dir/src/styles $dir/src/images
+echo "Используется для вывода текста и изображений" > $dir/README.md
+touch $dir/gitignore
 fi
 echo "Проект создан"
 exit 0
