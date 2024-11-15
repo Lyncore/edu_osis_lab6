61baaf51b652dc7cc7d22489c39b9e25ae1a16a6
rdydryliu <borisov.evan@gmail.com>
2024-11-15 21:07:47 +0900

    изменено:      auto.sh Все папки создаются при помощи одноц функции mkdir

--- 223/Borisov/auto.sh
+++ 223/Borisov/auto.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 if [ $# -eq 1 ]; then
 dir="$PWD/$1"
-mkdir -p $dir/src $dir/src/styles $dir/src/images
-echo "Используется для вывода текста и изображений" > $dir/README.md
-touch $dir/gitignore
 fi
 if [ $# -eq 2 ]; then
 dir="$2/$1"
+fi
 mkdir -p $dir/src $dir/src/styles $dir/src/images
 echo "Используется для вывода текста и изображений" > $dir/README.md
-touch $dir/gitignore
-fi
+touch $dir/.gitignore
 echo "Проект создан"
 exit 0
