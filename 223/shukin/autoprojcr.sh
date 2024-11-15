d53d9d66e0364bc2bf8366fc9a2f1b0778358ac3
Spets229 <andreyschukin163@gmail.com>
2024-11-15 21:36:44 +0700

    Изменён скрипт autoprojcr.sh

--- 223/shukin/autoprojcr.sh
+++ 223/shukin/autoprojcr.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+path=""
+
+#Проверка введено ли название проекта
 if [ -z "$1" ]
 then
   echo "Введите название проекта."
-  exit 0
+  exit 1
 fi
 
-if [ -d "$1" ] && [ -z "$2" ]
-then
-  echo "Проект с таким именем уже существует."
-  exit 0
-fi
-
+#Определение пути
 if [ -z "$2" ]
 then
-  mkdir -p "$1"/src/scripts "$1"/src/styles "$1"/src/images
-  touch "$1"/README.md "$1"/.gitignore
-  echo "Скопировать репозиторий при помощи команды git clone https://github.com/Spets229/edu_osis_lab6.git" >> "$1"/README.md
+  path="$1"
 else 
-  mkdir -p "$PWD"/"$2"/"$1"/src/scripts "$PWD"/"$2"/"$1"/src/styles "$PWD"/"$2"/"$1"/src/images 
-  touch "$PWD"/"$2"/"$1"/README.md "$PWD"/"$2"/"$1"/.gitignore 
-  echo "Скопировать репозиторий при помощи команды git clone https://github.com/Spets229/edu_osis_lab6.git" >> "$PWD"/"$2"/"$1"/README.md
+  path="$PWD/$2/$1"
 fi
+
+#Проверка существует ли такое название проекта
+if [ -d "$path" ]
+then
+  echo "Проект с таким именем уже существует."
+  exit 1
+fi
+
+#Создание папок и файлов
+mkdir -p $path/src/scripts $path/src/styles $path/src/images
+touch $path/README.md $path/.gitignore
+echo "Скопировать репозиторий при помощи команды git clone https://github.com/Spets229/edu_osis_lab6.git" >> $path/README.md
 
 echo "Проект $1 успешно создан."
 exit 0
