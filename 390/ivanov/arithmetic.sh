9e5d7a365addaa3ccaedcb9692d409aa7763dd3c
Kleozisss <vlad.turkivanov@gmail.com>
2024-12-19 18:03:25 +0700

    Добавлены скрипты в директорию 390/ivanov

---
+++ 390/ivanov/arithmetic.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+  echo "Использование: \$0 <число1> <число2> <операция>"
+  exit 1
+fi
+
+NUM1=\$1
+NUM2=\$2
+OPERATION=\$3
+
+case $OPERATION in
+  +)
+    echo "$((NUM1 + NUM2))"
+    ;;
+  -)
+    echo "$((NUM1 - NUM2))"
+    ;;
+  \*)
+    echo "$((NUM1 * NUM2))"
+    ;;
+  /)
+    if [ "$NUM2" -eq 0 ]; then
+      echo "Ошибка: Деление на ноль."
+    else
+      echo "$((NUM1 / NUM2))"
+    fi
+    ;;
+  *)
+    echo "Неизвестная операция."
+    ;;
+esac
